9b7428f438ad0c9385a67638182a775693cf4f37
Morgane <git@morgane.dev>
2025-01-15 09:04:20 +0100

    "Fixed" polybar not using main bar for vm screen

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,7 +6,7 @@ if type "xrandr"; then
 	PRIMARY_MONITOR=$(xrandr --query | grep " primary" | cut -d" " -f1)
 	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
 		i3-msg "workspace 1:$m"	
-		if [[ $m == "$PRIMARY_MONITOR" ]]; then
+		if [[ $m == "$PRIMARY_MONITOR" ]] || [[ $m == "Virtual-1" ]]; then
 			MONITOR=$m polybar --reload --config=/etc/nixos/config/polybar/config.ini example &
 		else
 			MONITOR=$m polybar --reload --config=/etc/nixos/config/polybar/config.ini secondary &
